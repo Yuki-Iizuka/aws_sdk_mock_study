7da2b8d21f92e6fb40fc58977125a46a41d1a5ca
Yuki-Iizuka <19604760+Yuki-Iizuka@users.noreply.github.com>
2022-01-24 10:55:54 +0900

    add AWS SDK

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+async function fetchBucketNames() {
+  const AWS = require('aws-sdk');
+  const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+  const result = await s3.listBuckets().promise();
+  return result.Buckets.map(bucket => bucket.Name);
+}
+
+(async () => {
+  const bucketNames = await fetchBucketNames();
+  console.log(bucketNames);
+})();
